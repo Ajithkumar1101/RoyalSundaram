ed7371fef256b264cae0e8ce85ae8834df7ada57
Ajithkumar1101 <103488005+Ajithkumar1101@users.noreply.github.com>
2022-11-01 12:17:33 +0530

    New project

---
+++ src/main/java/com/prodian/RoyalSundaramApplication.java
@@ -0,0 +1,13 @@
+package com.prodian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalSundaramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalSundaramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prodian/RoyalSundaramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prodian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyalSundaramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
