d91334eeca972ba303628df9d692a4e0a047236a
rohithprodian <rohith.vs@prodian.co.in>
2022-11-03 15:30:27 +0530

    changes 1

--- src/main/java/com/prodian/controller/QRController.java
+++ src/main/java/com/prodian/controller/QRController.java
@@ -1,6 +1,4 @@
 package com.prodian.controller;
- 
- 
 
 import com.google.zxing.WriterException;
 import com.prodian.QRcodeModel.qrcodemodel;
@@ -21,53 +19,48 @@ import java.util.Base64.Encoder;
 
 @RestController
 public class QRController {
-	
+
 	@Autowired
 	private QrCodeRepository qrCodeRepository;
 
- private static final String QR_CODE_IMAGE_PATH = "D://image.png";
+	private static final String QR_CODE_IMAGE_PATH = "D://image.png";
 
-    
- 
-    
-     @PostMapping("/createqr")
-    public qrcodemodel getQRCode (@RequestBody qrcodemodel model,Model modal){
+	@PostMapping("/createqr")
+	public qrcodemodel getQRCode(@RequestBody qrcodemodel model, Model modal) {
 
-    	String link = model.getQrLink();
-    	System.out.println(link + "demo hh23");
+		String link = model.getQrLink();
+		System.out.println(link + "demo hh23");
 
-    	String name=model.getQrname();
-    	qrcodemodel action = qrCodeRepository.save(model);
-    	
-    	qrcodemodel response = action;
-    	
+		String name = model.getQrname();
+		qrcodemodel action = qrCodeRepository.save(model);
 
-        byte[] image = new byte[0];
-        try {
-            // Convert Byte Array into Base64 Encode String
+		qrcodemodel response = action;
+
+		byte[] image = new byte[0];
+		try {
+			// Convert Byte Array into Base64 Encode String
 //          String qrcode = Base64.getEncoder().encodeToString(image);
-        	Encoder encoder = Base64.getEncoder();
-        	String originalString = link.split("=")[1];
-        	String encodedString = encoder.encodeToString(originalString.getBytes());
-        	link = link.split("=")[0]+"="+encodedString+"&&encoded=true";
-         
-            // Generate and Return Qr Code in Byte Array
-            image = QRCODEGenerator.getQRCodeImage(link,250,250);
+			Encoder encoder = Base64.getEncoder();
+			String originalString = link.split("=")[1];
+			String encodedString = encoder.encodeToString(originalString.getBytes());
+			link = link.split("=")[0] + "=" + encodedString + "&&encoded=true";
 
-            // Generate and Save Qr Code Image in static/image folder
-            QRCODEGenerator.generateQRCodeImage(link,250,250,"D://"+name+".png");
+			// Generate and Return Qr Code in Byte Array
+			image = QRCODEGenerator.getQRCodeImage(link, 250, 250);
 
-        } catch (WriterException | IOException e) {
-            e.printStackTrace();
-        }
+			// Generate and Save Qr Code Image in static/image folder
+			QRCODEGenerator.generateQRCodeImage(link, 250, 250, "D:\\QR\\DEM\\" + name + ".png");
 
+		} catch (WriterException | IOException e) {
+			e.printStackTrace();
+		}
 
-        modal.addAttribute("mediu",link);
-        modal.addAttribute("giub",link);
-        modal.addAttribute("qrde",link);
-        response.setQrId(action.getQrId());
-        response.setQrname(action.getQrname());
-        response.setQrLink(link);
-        return response;
-    }
+		modal.addAttribute(link);
+		modal.addAttribute(link);
+		modal.addAttribute(link);
+		response.setQrId(action.getQrId());
+		response.setQrname(action.getQrname());
+		response.setQrLink(link);
+		return response;
+	}
 }
